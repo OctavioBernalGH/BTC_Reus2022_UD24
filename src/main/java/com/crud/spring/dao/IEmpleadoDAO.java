92edfab3975c2f08e401bb16c30d5ee8b2571dd7
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-12 12:52:40 +0200

    Se modifica el dao.
    Se extiende de la clase JpaRepository.
    Se crea una lista de empleados retornados por nombre.

--- src/main/java/com/crud/spring/dao/IEmpleadoDAO.java
+++ src/main/java/com/crud/spring/dao/IEmpleadoDAO.java
@@ -1,5 +1,12 @@
 package com.crud.spring.dao;
 
-public interface IEmpleadoDAO {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.crud.spring.dto.Empleado;
+
+public interface IEmpleadoDAO extends JpaRepository<Empleado, Long>{
+	
+	// Lista los empleados por nombre
+	public List<Empleado> findByNombre(String nombre);
 
 }
