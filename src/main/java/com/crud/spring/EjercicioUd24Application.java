91879efd1f673012546ee28cb6e733050d5af703
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-12 11:51:56 +0200

    Initial commit

---
+++ src/main/java/com/crud/spring/EjercicioUd24Application.java
@@ -0,0 +1,13 @@
+package com.crud.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioUd24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioUd24Application.class, args);
+	}
+
+}
