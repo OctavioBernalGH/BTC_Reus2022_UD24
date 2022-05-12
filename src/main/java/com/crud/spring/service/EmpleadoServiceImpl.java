f6e7af99b5ef1ba396b54d062c3a1d68439d9688
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-12 13:33:16 +0200

    Se añade contenido en la clase service.
    Se añaden los métodos implementados por la interfaz en la clase service. Se llena el cuerpo de las funciones.

--- src/main/java/com/crud/spring/service/EmpleadoServiceImpl.java
+++ src/main/java/com/crud/spring/service/EmpleadoServiceImpl.java
@@ -1,5 +1,48 @@
 package com.crud.spring.service;
 
-public class EmpleadoServiceImpl {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.crud.spring.dao.IEmpleadoDAO;
+import com.crud.spring.dto.Empleado;
+
+public class EmpleadoServiceImpl implements IEmpleadoService{
+
+	@Autowired
+	IEmpleadoDAO iEmpleadoDAO;
+	@Override
+	public List<Empleado> listarEmpleados() {
+		// Devuelve una lista de Empleados.
+		return iEmpleadoDAO.findAll();
+	}
+
+	@Override
+	public Empleado guardarEmpleado(Empleado empleado) {
+		// Crear un empleado del tipo Empleado.
+		return iEmpleadoDAO.save(empleado);
+	}
+
+	@Override
+	public Empleado buscarEmpleadoId(long id) {
+		// Devuelve un empleado buscando por ID.
+		return iEmpleadoDAO.findById(id).get();
+	}
+
+	@Override
+	public List<Empleado> listarEmpleadoNombre(String nombre) {
+		// Lista/Buscar empleado por nombre.
+		return iEmpleadoDAO.findByNombre(nombre);
+	}
+
+	@Override
+	public Empleado actualizarEmpleado(Empleado empleado) {
+		// Actualiza un empleado.
+		return iEmpleadoDAO.save(empleado);
+	}
+
+	@Override
+	public void eliminarEmpleado(long id) {
+		// Eliminar empleado buscando por ID.
+		iEmpleadoDAO.deleteById(id);
+	}
 
 }
